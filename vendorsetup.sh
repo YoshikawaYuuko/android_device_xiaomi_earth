05a59f2fb9166e3eac234329addf3e9217402054
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-22 06:22:09 +0000

    vendorsetup: Change device_mediatek_sepolicy_vndr repo
    Signed-off-by: Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone https://github.com/YoshikawaYuuko/fortissimo.git -b keys vendor/lineag
 # Hardware Repos
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/YoshikawaYuuko/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
 
 # Common IMS
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
