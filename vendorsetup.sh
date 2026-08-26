9c18617015ef8b418b2addc69dcd768eaf2ebb21
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-26 17:05:41 +0700

    vendorsetup: change vendor tree branch
    Signed-off-by: Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo "Cloning Stuffs Needed For Earth"
 
 # Other earth stuffs
-git clone https://github.com/YoshikawaYuuko/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
+git clone https://github.com/YoshikawaYuuko/vendor_xiaomi_earth.git -b 16.2-ims vendor/xiaomi/earth
 git clone https://github.com/YoshikawaYuuko/kernel_xiaomi_earth.git -b lineage-23.2 kernel/xiaomi/earth
 git clone https://github.com/YoshikawaYuuko/fortissimo.git -b keys vendor/lineage-priv/keys
 
@@ -9,8 +9,5 @@ git clone https://github.com/YoshikawaYuuko/fortissimo.git -b keys vendor/lineag
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
-
-# Common IMS
-git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 echo "All Done!, Let's Start!"
