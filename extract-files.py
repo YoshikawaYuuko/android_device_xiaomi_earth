9bb1bb2039499951502b3ef1bb3f748638c18211
LuK1337 <priv.luk@gmail.com>
2026-08-29 12:44:01 +0700

    earth: apktool_patch() no longer takes custom arguments
    Change-Id: Idf85ae2cd0505c34e55f0e6acec2afc085372b78

--- extract-files.py
+++ extract-files.py
@@ -38,7 +38,7 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
-        .apktool_patch('blob-patches/ImsService.patch', '-r'),
+        .apktool_patch('blob-patches/ImsService.patch'),
     'system_ext/lib64/libsink.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
