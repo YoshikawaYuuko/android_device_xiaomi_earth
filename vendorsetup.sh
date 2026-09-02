b37806fff32ccf2cd6ceb2a08f9655e3b534c37d
Yuuko <oumaekumiko123@gmail.com>
2026-09-02 09:53:55 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,14 @@
 echo "Cloning Stuffs Needed For Earth"
 
 # Other earth stuffs
-git clone https://github.com/YoshikawaYuuko/vendor_xiaomi_earth.git -b 17.0 vendor/xiaomi/earth
-git clone https://github.com/YoshikawaYuuko/kernel_xiaomi_earth.git -b lineage-23.2 kernel/xiaomi/earth --depth=1
-git clone https://github.com/YoshikawaYuuko/fortissimo.git -b keys vendor/lineage-priv/keys
+git clone https://github.com/dreamsolister26/vendor_xiaomi_earth.git -b 17.0 vendor/xiaomi/earth
+git clone https://github.com/dreamsolister26/kernel_xiaomi_earth.git -b lineage-23.2 kernel/xiaomi/earth --depth=1
+git clone https://github.com/dreamsolister26/fortissimo.git -b keys vendor/lineage-priv/keys
 
 # Hardware Repos
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
-git clone https://github.com/YoshikawaYuuko/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/dreamsolister26/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
 
 # Common IMS
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
